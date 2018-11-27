0e5573828340ff060e27f48710e8f735781f45cc
Ben Vankat <bvankat@gmail.com>
2018-11-27 12:54:13 -0600

    latest update

---
+++ data.js
@@ -0,0 +1,54 @@
+{
+    "bracketproject": {
+        "matrix_count": "12",
+        "total_brackets": "48"
+    },
+    "espn": {
+        "bpi": "48",
+        "chance_sweet_16": "11.2%",
+        "proj_tourney_seed": "6",
+        "proj_win_loss_conf": "9.3-8.7",
+        "proj_win_loss_overall": "18.9-12.1",
+        "seven_day_change": "5",
+        "sor": "22",
+        "sos": "68"
+    },
+    "kenpom": {
+        "rating": "34",
+        "sos": "74"
+    },
+    "massey": {
+        "composite": "32"
+    },
+    "ncaa": {
+        "net_rank": "36"
+    },
+    "sagarin": {
+        "sagarin_rate": "39"
+    },
+    "teamrankings": {
+        "make_tournament": "72.8%",
+        "most_likely_seed": "#6 (9.9%)",
+        "projected_losses": "11.0",
+        "projected_wins": "20.0",
+        "rank": "35",
+        "sos": "29"
+    },
+    "time": {
+        "time": "Nov 27, 12:54 PM"
+    },
+    "trank": {
+        "current_record": "5-1",
+        "trank": "43",
+        "trank_proj_record": "19-12 (9-9)"
+    },
+    "warrennolan": {
+        "current_conf_record": "0-0",
+        "group1": "0-1",
+        "group2": "2-0",
+        "group3": "1-0",
+        "group4": "2-0",
+        "rpi": "27",
+        "sos": "35"
+    }
+}
