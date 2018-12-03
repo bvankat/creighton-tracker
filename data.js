508256427d581d04a50f0cc93749e1c28d6481af
Ben Vankat <bvankat@gmail.com>
2018-12-02 21:33:49 -0600

    latest update

--- data.js
+++ data.js
@@ -4,51 +4,51 @@
         "total_brackets": "48"
     },
     "espn": {
-        "bpi": "45",
-        "chance_sweet_16": "13.6%",
-        "proj_tourney_seed": "6",
-        "proj_win_loss_conf": "9.6-8.4",
-        "proj_win_loss_overall": "19.4-11.6",
-        "seven_day_change": "2",
-        "sor": "22",
-        "sos": "83"
+        "bpi": "47",
+        "chance_sweet_16": "12.2%",
+        "proj_tourney_seed": "10",
+        "proj_win_loss_conf": "9.5-8.5",
+        "proj_win_loss_overall": "18.9-12.1",
+        "seven_day_change": "1",
+        "sor": "39",
+        "sos": "55"
     },
     "kenpom": {
-        "rating": "30",
-        "sos": "73"
+        "rating": "31",
+        "sos": "31"
     },
     "massey": {
         "composite": "33"
     },
     "ncaa": {
-        "net_rank": "34"
+        "net_rank": "38"
     },
     "sagarin": {
-        "sagarin_rate": "33"
+        "sagarin_rate": "39"
     },
     "teamrankings": {
-        "make_tournament": "81.2%",
-        "most_likely_seed": "#6 (11.7%)",
-        "projected_losses": "10.0",
-        "projected_wins": "21.0",
-        "rank": "28",
-        "sos": "34"
+        "make_tournament": "78.1%",
+        "most_likely_seed": "#6 (11.4%)",
+        "projected_losses": "10.6",
+        "projected_wins": "20.4",
+        "rank": "29",
+        "sos": "20"
     },
     "time": {
-        "time": "Nov 30, 12:16 PM"
+        "time": "Dec 02,  9:33 PM"
     },
     "trank": {
-        "current_record": "6-1",
-        "trank": "38",
+        "current_record": "6-2",
+        "trank": "33",
         "trank_proj_record": "20-11 (10-8)"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
-        "group1": "1-0",
-        "group2": "2-1",
-        "group3": "1-0",
+        "group1": "0-2",
+        "group2": "2-0",
+        "group3": "2-0",
         "group4": "2-0",
-        "rpi": "13",
-        "sos": "15"
+        "rpi": "20",
+        "sos": "8"
     }
 }
