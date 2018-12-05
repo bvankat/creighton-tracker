ae0083b3a53717ee5d36ab55dc5773848406b4f9
Ben Vankat <bvankat@gmail.com>
2018-12-05 11:09:59 -0600

    latest update

--- data.js
+++ data.js
@@ -1,58 +1,58 @@
 {
     "bracketproject": {
-        "matrix_count": "12",
-        "total_brackets": "48"
+        "matrix_count": "27",
+        "total_brackets": "28"
     },
     "espn": {
-        "bpi": "47",
-        "chance_sweet_16": "11.6%",
+        "bpi": "46",
+        "chance_sweet_16": "11.4%",
         "proj_tourney_seed": "10",
-        "proj_win_loss_conf": "9.4-8.6",
-        "proj_win_loss_overall": "18.9-12.1",
-        "seven_day_change": "1",
+        "proj_win_loss_conf": "9.5-8.5",
+        "proj_win_loss_overall": "18.8-12.2",
+        "seven_day_change": "2",
         "sor": "37",
-        "sos": "52"
+        "sos": "57"
     },
     "kenpom": {
-        "rating": "31",
+        "rating": "29",
         "sos": "30"
     },
     "massey": {
         "composite": "27"
     },
     "ncaa": {
-        "net_rank": "47"
+        "net_rank": "44"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "40"
+        "sagarin_rate": "41"
     },
     "teamrankings": {
-        "make_tournament": "77.9%",
-        "most_likely_seed": "#6 (11.5%)",
-        "projected_losses": "10.6",
-        "projected_wins": "20.4",
-        "rank": "29",
-        "sos": "19"
+        "make_tournament": "80.1%",
+        "most_likely_seed": "#6 (11.7%)",
+        "projected_losses": "10.5",
+        "projected_wins": "20.5",
+        "rank": "28",
+        "sos": "20"
     },
     "time": {
-        "time": "Dec 03, 11:42 PM"
+        "time": "Dec 05, 11:09 AM"
     },
     "trank": {
         "current_record": "6-2",
-        "trank": "33",
+        "trank": "34",
         "trank_proj_record": "20-11 (10-8)"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
-        "group1": "0-2",
-        "group2": "2-0",
+        "group1": "1-2",
+        "group2": "1-0",
         "group3": "2-0",
         "group4": "2-0",
-        "rpi": "22",
-        "sos": "6"
+        "rpi": "16",
+        "sos": "4"
     }
 }
