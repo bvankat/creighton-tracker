6dfa9886848ab1fc1e19bc036c54ce28390f2a9d
Ben Vankat <bvankat@gmail.com>
2018-12-16 22:40:10 -0600

    latest update

--- data.js
+++ data.js
@@ -1,61 +1,61 @@
 {
     "bracketproject": {
         "matrix_count": "27",
-        "total_brackets": "28"
+        "total_brackets": "38"
     },
     "espn": {
-        "bpi": "50",
-        "chance_sweet_16": "8.5%",
-        "proj_tourney_seed": "13",
-        "proj_win_loss_conf": "9.0-9.0",
-        "proj_win_loss_overall": "18.1-12.9",
-        "seven_day_change": "-5",
-        "sor": "50",
-        "sos": "26"
+        "bpi": "52",
+        "chance_sweet_16": "7.5%",
+        "proj_tourney_seed": "12",
+        "proj_win_loss_conf": "9.2-8.8",
+        "proj_win_loss_overall": "18.4-12.6",
+        "seven_day_change": "-2",
+        "sor": "45",
+        "sos": "45"
     },
     "kenpom": {
-        "rating": "39",
-        "sos": "20"
+        "rating": "38",
+        "sos": "43"
     },
     "kpi_sports": {
-        "kpi_ranking": "37"
+        "kpi_ranking": "40"
     },
     "massey": {
         "composite": "39"
     },
     "ncaa": {
-        "net_rank": "58"
+        "net_rank": "60"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "48"
+        "sagarin_rate": "50"
     },
     "teamrankings": {
-        "make_tournament": "67.1%",
-        "most_likely_seed": "#6 (8.4%)",
-        "projected_losses": "11.5",
-        "projected_wins": "19.5",
-        "rank": "41",
-        "sos": "14"
+        "make_tournament": "68.1%",
+        "most_likely_seed": "#6 (8.9%)",
+        "projected_losses": "11.3",
+        "projected_wins": "19.7",
+        "rank": "40",
+        "sos": "18"
     },
     "time": {
-        "time": "Dec 14,  9:12 AM"
+        "time": "Dec 16, 10:40 PM"
     },
     "trank": {
-        "current_record": "6-3",
-        "trank": "49",
+        "current_record": "7-3",
+        "trank": "48",
         "trank_proj_record": "19-12 (10-8)"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
         "group1": "0-3",
-        "group2": "1-0",
+        "group2": "2-0",
         "group3": "3-0",
         "group4": "2-0",
-        "rpi": "28",
-        "sos": "6"
+        "rpi": "40",
+        "sos": "22"
     }
 }
