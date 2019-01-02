e159ee27c5f27fa574581114e6d0867be350de79
Ben Vankat <bvankat@gmail.com>
2019-01-01 20:23:38 -0600

    latest update

--- data.js
+++ data.js
@@ -1,62 +1,62 @@
 {
     "bracketproject": {
-        "matrix_count": "25",
-        "matrix_seed": "11",
+        "matrix_count": "24",
+        "matrix_seed": "10",
         "total_brackets": "35"
     },
     "espn": {
-        "bpi": "41",
-        "chance_sweet_16": "9.2%",
-        "proj_tourney_seed": "11",
-        "proj_win_loss_conf": "10.0-8.0",
-        "proj_win_loss_overall": "19.0-12.0",
-        "seven_day_change": "8",
-        "sor": "44",
-        "sos": "25"
+        "bpi": "31",
+        "chance_sweet_16": "13.3%",
+        "proj_tourney_seed": "9",
+        "proj_win_loss_conf": "11.3-6.7",
+        "proj_win_loss_overall": "20.3-10.7",
+        "seven_day_change": "18",
+        "sor": "33",
+        "sos": "14"
     },
     "kenpom": {
-        "rating": "35",
-        "sos": "24"
+        "rating": "30",
+        "sos": "12"
     },
     "kpi_sports": {
-        "kpi_ranking": "47"
+        "kpi_ranking": "28"
     },
     "massey": {
         "composite": "32"
     },
     "ncaa": {
-        "net_rank": "47"
+        "net_rank": "37"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "40"
+        "sagarin_rate": "32"
     },
     "teamrankings": {
-        "make_tournament": "75.4%",
+        "make_tournament": "84.9%",
         "most_likely_seed": "6",
-        "projected_losses": "11.2",
-        "projected_wins": "19.8",
-        "rank": "30",
-        "sos": "13"
+        "projected_losses": "10.4",
+        "projected_wins": "20.6",
+        "rank": "29",
+        "sos": "9"
     },
     "time": {
-        "time": "Dec 31,  1:41 PM"
+        "time": "Jan 01,  8:23 PM"
     },
     "trank": {
-        "current_record": "9-4",
-        "trank": "40",
-        "trank_proj_record": "19-12 (10-8)",
-        "trank_seed": "9"
+        "current_record": "10-4",
+        "trank": "32",
+        "trank_proj_record": "20-11 (11-7)",
+        "trank_seed": "8"
     },
     "warrennolan": {
-        "current_conf_record": "0-0",
-        "group1": "0-4",
+        "current_conf_record": "1-0",
+        "group1": "1-4",
         "group2": "1-0",
         "group3": "4-0",
         "group4": "3-0",
-        "sos": "8"
+        "sos": "6"
     }
 }
