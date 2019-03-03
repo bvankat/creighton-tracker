3fed4c152ab5c399329ff41dad2988c30cc2f96a
Ben Vankat <bvankat@gmail.com>
2019-03-03 15:55:57 -0600

    latest update

--- data.js
+++ data.js
@@ -2,33 +2,33 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": 0,
-        "total_brackets": "100"
+        "total_brackets": "105"
     },
     "cbs": {
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "47",
-        "chance_sweet_16": "0.8%",
+        "bpi": "48",
+        "chance_sweet_16": "0.5%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "7.6-10.4",
-        "proj_win_loss_overall": "16.6-14.4",
-        "seven_day_change": "0",
-        "sor": "69",
-        "sos": "22"
+        "proj_win_loss_conf": "7.9-10.1",
+        "proj_win_loss_overall": "16.9-14.1",
+        "seven_day_change": "-4",
+        "sor": "65",
+        "sos": "41"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "kenpom": {
-        "rating": "51",
-        "sos": "23"
+        "rating": "52",
+        "sos": "29"
     },
     "kpi_sports": {
-        "kpi_ranking": "50"
+        "kpi_ranking": "51"
     },
     "massey": {
-        "composite": "61"
+        "composite": "54"
     },
     "ncaa": {
         "net_rank": "57"
@@ -41,29 +41,29 @@
         "sagarin_rate": "49"
     },
     "teamrankings": {
-        "make_tournament": "14.6%",
-        "most_likely_seed": "12",
-        "projected_losses": "14.4",
-        "projected_wins": "16.6",
+        "make_tournament": "16.7%",
+        "most_likely_seed": "11",
+        "projected_losses": "14.2",
+        "projected_wins": "16.8",
         "rank": "42",
-        "sos": "31"
+        "sos": "39"
     },
     "time": {
-        "time": "Feb 23,  9:23 PM"
+        "time": "Mar 03,  3:55 PM"
     },
     "trank": {
         "current_record": "15-13",
-        "trank": "46",
+        "trank": "49",
         "trank_proj_record": "17-14 (8-10)",
         "trank_seed": "NR"
     },
     "warrennolan": {
         "current_conf_record": "6-9",
-        "group1": "3-9",
-        "group2": "4-4",
-        "group3": "4-0",
+        "group1": "2-10",
+        "group2": "6-3",
+        "group3": "3-0",
         "group4": "3-0",
-        "rpi": "67",
-        "sos": "15"
+        "rpi": "72",
+        "sos": "17"
     }
 }
