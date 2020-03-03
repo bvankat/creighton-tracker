280b1c0396a48d2c5a2b70bdcb0ae1539ac0f627
Ben Vankat <bvankat@gmail.com>
2020-03-03 00:46:12 -0600

    latest update

--- data.js
+++ data.js
@@ -1,69 +1,63 @@
 {
     "bracketproject": {
-        "matrix_count": "33",
-        "matrix_seed": "NR",
-        "total_brackets": "87"
+        "matrix_count": "108",
+        "matrix_seed": "Creighton",
+        "total_brackets": "108"
     },
     "cbs": {
-        "cbs_rank": "12-seed"
+        "cbs_rank": "3-seed"
     },
     "espn": {
-        "bpi": "43",
-        "chance_sweet_16": "2.5%",
-        "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "9.0-9.0",
-        "proj_win_loss_overall": "18.6-13.4",
-        "seven_day_change": "0",
-        "sor": "55",
-        "sos": "46"
+        "bpi": "27",
+        "chance_sweet_16": "30.2%",
+        "proj_tourney_seed": "3",
+        "proj_win_loss_conf": "12.5-5.5",
+        "proj_win_loss_overall": "23.5-7.5",
+        "seven_day_change": "-3",
+        "sor": "9",
+        "sos": "20"
     },
     "espn_bracketology": {
-        "seed": "NR"
+        "seed": "4-seed"
     },
     "kenpom": {
-        "rating": "49",
-        "sos": "43"
-    },
-    "kpi_sports": {
-        "kpi_ranking": "41"
-    },
-    "massey": {
-        "composite": "44"
+        "rating": "18",
+        "sos": "14"
     },
     "ncaa": {
-        "net_rank": "54"
+        "net_rank": "13"
     },
     "polls": {
-        "ap_rank": "NR",
-        "coaches_rank": "NR"
+        "ap_rank": "11",
+        "coaches_rank": "14"
     },
     "sagarin": {
-        "sagarin_rate": "42"
+        "sagarin_rate": "18  Creighton"
     },
     "teamrankings": {
-        "make_tournament": "32.1%",
-        "most_likely_seed": "11",
-        "projected_losses": "13.0",
-        "projected_wins": "18.0",
-        "rank": "38",
-        "sos": "41"
+        "make_tournament": "100.0%",
+        "most_likely_seed": "3",
+        "projected_losses": "7.6",
+        "projected_wins": "23.4",
+        "rank": "23",
+        "sos": "18"
     },
     "time": {
-        "time": "Mar 11,  1:00 PM"
+        "time": "Mar 03, 12:46 AM"
     },
     "trank": {
-        "current_record": "18-13",
-        "trank": "45",
-        "trank_proj_record": "19-13 (9-9)",
+        "current_record": "22-7",
+        "trank": "7",
+        "trank_proj_record": "24-7 (13-5)",
         "trank_seed": "NR"
     },
     "warrennolan": {
-        "current_conf_record": "9-9",
-        "group1": "3-10",
-        "group2": "6-3",
-        "group3": "5-0",
-        "group4": "3-0",
-        "rpi": "50",
-        "sos": "14"
+        "current_conf_record": "11-5",
+        "group1": "7-5",
+        "group2": "5-2",
+        "group3": "4-0",
+        "group4": "5-0",
+        "rpi": "12",
+        "sos": "18"
     }
 }
