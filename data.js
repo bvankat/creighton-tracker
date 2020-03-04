43bee58e153188be1c50f7aecec5b2d8f5a1e445
Ben Vankat <bvankat@gmail.com>
2020-03-04 12:20:42 -0600

    latest update

--- data.js
+++ data.js
@@ -1,28 +1,28 @@
 {
     "bracketproject": {
-        "matrix_count": "108",
+        "matrix_count": "136",
         "matrix_seed": "Creighton",
-        "total_brackets": "108"
+        "total_brackets": "136"
     },
     "cbs": {
         "cbs_rank": "3-seed"
     },
     "espn": {
-        "bpi": "27",
-        "chance_sweet_16": "30.2%",
-        "proj_tourney_seed": "3",
+        "bpi": "28",
+        "chance_sweet_16": "30.1%",
+        "proj_tourney_seed": "2",
         "proj_win_loss_conf": "12.5-5.5",
         "proj_win_loss_overall": "23.5-7.5",
-        "seven_day_change": "-3",
-        "sor": "9",
-        "sos": "20"
+        "seven_day_change": "-4",
+        "sor": "8",
+        "sos": "23"
     },
     "espn_bracketology": {
         "seed": "4-seed"
     },
     "kenpom": {
         "rating": "18",
-        "sos": "14"
+        "sos": "15"
     },
     "ncaa": {
         "net_rank": "13"
@@ -32,18 +32,18 @@
         "coaches_rank": "14"
     },
     "sagarin": {
-        "sagarin_rate": "18  Creighton"
+        "sagarin_rate": "19"
     },
     "teamrankings": {
         "make_tournament": "100.0%",
         "most_likely_seed": "3",
         "projected_losses": "7.6",
         "projected_wins": "23.4",
-        "rank": "23",
+        "rank": "22",
         "sos": "18"
     },
     "time": {
-        "time": "Mar 03, 12:46 AM"
+        "time": "Mar 04, 12:20 PM"
     },
     "trank": {
         "current_record": "22-7",
@@ -53,11 +53,11 @@
     },
     "warrennolan": {
         "current_conf_record": "11-5",
-        "group1": "7-5",
-        "group2": "5-2",
+        "group1": "6-5",
+        "group2": "6-2",
         "group3": "4-0",
         "group4": "5-0",
-        "rpi": "12",
-        "sos": "18"
+        "rpi": "11",
+        "sos": "16"
     }
 }
